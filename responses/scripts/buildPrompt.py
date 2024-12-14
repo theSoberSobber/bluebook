f4c33f91125a23ba35a9d60b2213a10046f88606
theSoberSobber <akshatshrivastava0010@gmail.com>
2024-12-14 23:19:18 +0530

    [BUG] Fix Prompt Build

--- responses/scripts/buildPrompt.py
+++ responses/scripts/buildPrompt.py
@@ -33,17 +33,12 @@ file_path = get_latest_response_file(responses_folder=responses_folder)
 with open(file_path, 'r') as f:
     pr_data = json.load(f)
 
-if not pr_data["email"].endswith(".ac.in"):
-    moderation_output = "No, Unauthorized Email."
-    with open('/tmp/moderation_result.txt', 'w') as result_file:
-        result_file.write(moderation_output)
-else:
-    qa_data = pr_data["data"]
+qa_data = pr_data["data"]
 
-    prompt = "You are given a bunch of questions and answers pertaining to a document that contains interview transcripts for candidates that have been selected into companies, please flag the document into suitable for publishing on a public facing site or not. Provide a one liner reason flagging the parts that are problematic. We are defining problematic here as profanities/slurs etc. The text can be flagged if the content doesn't seem relevant to our interview experience publishing site too (basically we are trying to avoid spam and hateful speech). The text can be multilingual. You must output in this format: <Yes/No>. <Reasoning Highlighting the Problematic Parts if No>. Please note that we are not expected an evaluation for each question, just one singular evaluation for the whole thing in the given input format.\n\n"
+prompt = "You are given a bunch of questions and answers pertaining to a document that contains interview transcripts for candidates that have been selected into companies, please flag the document into suitable for publishing on a public facing site or not. Provide a one liner reason flagging the parts that are problematic. We are defining problematic here as profanities/slurs etc. The text can be flagged if the content doesn't seem relevant to our interview experience publishing site too (basically we are trying to avoid spam and hateful speech). The text can be multilingual. You must output in this format: <Yes/No>. <Reasoning Highlighting the Problematic Parts if No>. Please note that we are not expected an evaluation for each question, just one singular evaluation for the whole thing in the given input format.\n\n"
 
-    for question, answer in qa_data.items():
-        if not len(answer): continue
-        prompt += f"Question: {question}\nAnswer: {', '.join(answer)}\n\n"
+for question, answer in qa_data.items():
+    if not len(answer): continue
+    prompt += f"Question: {question}\nAnswer: {', '.join(answer)}\n\n"
 
 print(prompt)

dc00ea45826ad477c10bc9b8d8ace50628c2d92b
theSoberSobber <akshatshrivastava0010@gmail.com>
2024-12-18 23:41:02 +0530

    [MNT] Clean GH-Pages Branch

--- bluebook/public/sw.js
+++
@@ -1,7 +0,0 @@
-importScripts(
-    'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
-);
-
-const { registerRoute } =  workbox.routing;
-const { precacheAndRoute } = workbox.precaching;
-
